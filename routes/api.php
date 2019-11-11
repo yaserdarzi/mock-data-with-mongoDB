1c129523fc9e6f32347260fa5a3030809811ede0
Yaser Darzi <yaser.darzi@gmail.com>
2019-11-11 15:25:59 +0330

    fix company Type

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,22 @@ Route::namespace('Api\V1')->prefix('/v1/')->group(function () {
     Route::resource('person', 'PersonController');
 
     //Company
+    Route::get('company/type', function () {
+        $companyType = [
+            ['id' => 1, 'title' => 'سهامی عام'],
+            ['id' => 2, 'title' => 'سهامی خاص'],
+            ['id' => 3, 'title' => 'مسئولیت محدود'],
+            ['id' => 4, 'title' => 'تضامنی'],
+            ['id' => 5, 'title' => 'تعاونی'],
+            ['id' => 6, 'title' => 'سازمان'],
+            ['id' => 7, 'title' => 'اداره کل'],
+            ['id' => 8, 'title' => 'وزارتخانه'],
+            ['id' => 9, 'title' => 'ارگان دولتی'],
+            ['id' => 10, 'title' => 'سایر'],
+            ['id' => 11, 'title' => 'سهامی']
+        ];
+        return response()->json($companyType);
+    });
     Route::resource('company', 'CompanyController');
 
     //Cost Center
@@ -35,3 +51,4 @@ Route::namespace('Api\V1')->prefix('/v1/')->group(function () {
     Route::resource('geolocation', 'GeolocationController');
 
 });
+//            , '', '', '', '', '', '', '', ' ', ''
