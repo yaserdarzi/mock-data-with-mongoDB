b9a240b985497987507f7c00ba5409a8a73019c2
Yaser Darzi <yaser.darzi@gmail.com>
2019-11-30 16:20:01 +0330

    fix Detail Account Type

--- app/Http/Controllers/Api/V1/DetailAccountTypeController.php
+++ app/Http/Controllers/Api/V1/DetailAccountTypeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Exceptions\ApiException;
 use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
